807e44d39920c39653bfb6b885f05c7255030a7d
Mouad Ennaciri <mouad.ennaciri.1@ulaval.ca>
2024-09-26 10:50:13 -0400

    exercice1

--- src/main/java/org/example/Exercice1.java
+++ src/main/java/org/example/Exercice1.java
@@ -1,18 +1,14 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Exercice1 {
 
     public List<Salary> getAllWorkerSalaries(List<Worker> workers) {
         List<Salary> salaries = new LinkedList<>();
         for (Worker worker : workers) {
-            if (worker.getType() == WorkerType.HOURLY) {
-                double hourlyWage = worker.getHourlyWage();
-                double hours = worker.getWorkedHours();
-                salaries.add(new Salary(hourlyWage * hours));
-            } else {
-                double salary = worker.getAnnualSalary() / 52.0;
-                salaries.add(new Salary(salary));
-            }
+            salaries.add(worker.getSallary());
         }
 
         return salaries;
---
+++ src/main/java/org/example/WorkerType.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public enum WorkerType {
+    HOURLY,
+    SALARIED
+}
